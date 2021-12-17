86c51aca13ecc650f83caaa80662efacdad5f10d
BlueWorldInc <keamybakunin@gmail.com>
2021-12-17 16:26:52 +0100

    created the first neural network object

--- main.py
+++ main.py
@@ -2,7 +2,13 @@
 class neuralNetwork:
 
 	# initialise the neural network
-	def __init__(self):
+	def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
+		# set number of nodes in each input, hidden and output layers
+		self.inodes = inputNodes
+		self.hnodes = hiddenNodes
+		self.onodes = outputNodes
+		# set learning rate
+		self.lr = learningRate
 		pass
 
 	# train the neural entwork
@@ -12,3 +18,10 @@ class neuralNetwork:
 	# query the neural network
 	def query():
 		pass
+
+inputNodes = 3
+hiddenNodes = 3
+outputNodes = 3
+learningRate = 0.5
+
+n = neuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
