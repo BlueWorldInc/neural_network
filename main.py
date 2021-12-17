0a2c92885629a45c3b69965a8772a04c92d49ed5
BlueWorldInc <keamybakunin@gmail.com>
2021-12-17 16:22:50 +0100

    sceleton of neural network class done

---
+++ main.py
@@ -0,0 +1,14 @@
+# neural network class definition
+class neuralNetwork:
+
+	# initialise the neural network
+	def __init__(self):
+		pass
+
+	# train the neural entwork
+	def train():
+		pass
+
+	# query the neural network
+	def query():
+		pass
---
+++ testlibrary.py
@@ -0,0 +1,12 @@
+# # TEST OF NUMPY AND MATPLOTLIB
+# import numpy
+# import matplotlib.pyplot as plt
+
+# a = numpy.zeros( [3,2] )
+# a[0,0] = 1
+# a[0,1] = 2
+# a[1,0] = 9
+# a[2,1] = 12
+
+# plt.imshow(a, interpolation="nearest")
+# plt.show()
