62a064e6c90a4e1428e74480fdc23838af7cb91b
BlueWorldInc <keamybakunin@gmail.com>
2022-01-24 20:17:30 +0100

    save

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import numpy
+import sys
 import scipy.special
 import matplotlib.pyplot as plt
+from matplotlib.backend_bases import MouseButton
 # %matplotlib inline
+
+numpy.set_printoptions(threshold=sys.maxsize)
 
 # neural network class definition
 class neuralNetwork:
@@ -21,6 +25,19 @@ class neuralNetwork:
 
 		self.activation_function = lambda x: scipy.special.expit(x)
 
+		pass
+
+	def save(self):
+		weigthsFile = open('./weigths/weigths.txt', 'w')
+		weigthsFile.write(str((n.wih.tolist())))
+		weigthsFile.write(str((n.who.tolist())))
+		pass
+
+	def load(self):
+		weigthsFile = open('./weigths/weigths.txt')
+		weigthsFileContent = weigthsFile.read()
+		self.wih = numpy.array(eval(weigthsFileContent.split(']]')[0] + ']]'))
+		self.who = numpy.array(eval(weigthsFileContent.split(']]')[1] + ']]'))
 		pass
 
 	# train the neural entwork
@@ -54,24 +71,28 @@ class neuralNetwork:
 
 #Init
 
-# inputNodes = 784
-# hiddenNodes = 200
-# outputNodes = 10
-# learningRate = 0.1
+inputNodes = 784
+hiddenNodes = 200
+outputNodes = 10
+learningRate = 0.1
 
-# n = neuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
+n = neuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
 
-# #Train
+# Load Data
 
-# training_data_file = open("mnist_dataset/mnist_train_100.csv", 'r')
-# training_data_list = training_data_file.readlines()
-# training_data_file.close()
+training_data_file = open("mnist_dataset/mnist_train.csv", 'r')
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+
+# Show Data
 
 # all_values = training_data_list[1].split(',')
 # print(all_values[0])
 # image_array = numpy.asfarray(all_values[1:]).reshape((28, 28))
 # plt.imshow(image_array, cmap='Greys', interpolation='None')
 # plt.show()
+
+# Train
 
 # epochs = 1
 # i = 0
@@ -90,30 +111,64 @@ class neuralNetwork:
 # 		pass
 # 	pass
 
-# print("done")
+print("done")
+n.load()
 
 #Testing
 
-# test_data_file = open("mnist_dataset/mnist_test.csv", 'r')
-# test_data_list = test_data_file.readlines()
-# test_data_file.close()
+test_data_file = open("mnist_dataset/mnist_test.csv", 'r')
+test_data_list = test_data_file.readlines()
+test_data_file.close()
 
-# scoreboard = []
+scoreboard = []
+wrong_records = [] 
 
-# for record in test_data_list:
-# 	all_values = record.split(',')
-# 	correct_label = int(all_values[0])
-# 	inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-# 	outputs = n.query(inputs)
-# 	label = numpy.argmax(outputs)
-# 	if (label == correct_label):
-# 		scoreboard.append(1)
-# 	else:
-# 		scoreboard.append(0)
-# 		pass
-# 	pass
+for record in test_data_list:
+	all_values = record.split(',')
+	correct_label = int(all_values[0])
+	inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+	outputs = n.query(inputs)
+	label = numpy.argmax(outputs)
+	if (label == correct_label):
+		scoreboard.append(1)
+	else:
+		scoreboard.append(0)
+		wrong_records.append([record, outputs])
+		# print(all_values[0])
+		# image_array = numpy.asfarray(all_values[1:]).reshape((28, 28))
+		# plt.imshow(image_array, cmap='Greys', interpolation='None')
+		# plt.show()
+		pass
+	pass
 
-# scorecard_array = numpy.asarray(scoreboard)
-# # print(scoreboard)
-# print("Performance = ", scorecard_array.sum() / scorecard_array.size)
+scorecard_array = numpy.asarray(scoreboard)
+# print(scoreboard)
+print("Performance = ", scorecard_array.sum() / scorecard_array.size)
+
+# Showcase
+
+plt.ion()
+offset = 0
+
+def on_click(event):
+	global offset
+	if event.button is MouseButton.LEFT:
+		offset += 1
+		showNumber(wrong_records[offset])
+	if event.button is MouseButton.RIGHT:
+		offset -= 1
+		showNumber(wrong_records[offset])
+
+def showNumber(wrong_record):
+	plt.clf()
+	all_values = wrong_record[0].split(',')
+	print(wrong_record[1])
+	print("Neural network:" + str(numpy.argmax(wrong_record[1])))
+	print("Real value:" + str(all_values[0]))
+	image_array = numpy.asfarray(all_values[1:]).reshape((28, 28))
+	plt.imshow(image_array, cmap='Greys', interpolation='None')
+	plt.show(block=True)
+
+plt.connect('button_press_event', on_click)
+showNumber(wrong_records[offset])
 
--- visualize.py
+++ visualize.py
@@ -1,55 +1,68 @@
 from main import *
+from matplotlib.backend_bases import MouseButton
 import sys
 
-def wait():
+waiting = True
+offset = 0
+
+def waitUntilClick():
 	while plt.waitforbuttonpress():
 		break
+	# while waiting:
+		# pass
+		# break
+		# plt.pause(0.1)
+		# if ()
+
+def on_click(event):
+	global offset
+	if event.button is MouseButton.LEFT:
+	# 	# print('disconnecting callback')
+	# 	# waiting = not waiting
+		offset += 1
+		showNumber(training_data_list[offset])
+	if event.button is MouseButton.RIGHT:
+	# 	# print('disconnecting callback')
+	# 	# waiting = not waiting
+		offset -= 1
+		showNumber(training_data_list[offset])
 
 def showNumber(number_line):
+	global offset
+	current_offset = offset
+	# waiting = True
 	plt.clf()
 	all_values = number_line.split(',')
 	image_array = numpy.asfarray(all_values[1:]).reshape((28, 28))
 	plt.imshow(image_array, cmap='Greys', interpolation='None')
-	plt.show()
-	wait()
+	plt.show(block=True)
+	# while current_offset == offset:
+		# plt.pause(0.1)
+	# else:
+		# showNumber(training_data_list[offset])
+	# waitUntilClick()
+	# plt.pause(1)
+	# print("1")
 
-#Init
-# inputNodes = 784
-# hiddenNodes = 200
-# outputNodes = 10
-# learningRate = 0.1
-
-# n = neuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
 number = 0
 if (len(sys.argv) > 1):
 	number = int(sys.argv[1])
 
 plt.ion()
 
-#Train
 training_data_file = open("mnist_dataset/mnist_train_100.csv", 'r')
 training_data_list = training_data_file.readlines()
 training_data_file.close()
+plt.connect('button_press_event', on_click)
+# waitUntilClick()
+showNumber(training_data_list[offset])
+# for numb in range(2):
+	# showNumber(training_data_list[numb])
 
-# all_values = training_data_list[number].split(',')
-# print(all_values[0])
-# image_array = numpy.asfarray(all_values[1:]).reshape((28, 28))
-# plt.imshow(image_array, cmap='Greys', interpolation='None')
 
-# plt.draw(block=True)
-# plt.show()
-# press = plt.waitforbuttonpress(0.1)
-
-# wait()
-
-showNumber(training_data_list[number])
-showNumber(training_data_list[number+1])
-
-# plt.clf()
-
-# all_values = training_data_list[number+1].split(',')
-# image_array = numpy.asfarray(all_values[1:]).reshape((28, 28))
-# plt.imshow(image_array, cmap='Greys', interpolation='None')
-# plt.show()
-# plt.pause(1)
-
+# montrer une image
+# attendre pour un click
+# lors du click afficher une autre image (maj)
+# si click gauche ca augmente, si click gauche ca diminue
+# si click fleche ca augmente, si fleche gauche ca diminue
+# si on click sur fermer ca stop le programme
